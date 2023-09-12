83470098c6b32bf7f06264f4c9797a6e0015da6d
OppedRawen <yud2372@gmail.com>
2023-09-12 03:29:57 -0700

    fix?

--- src/footer.jsx
+++ src/footer.jsx
@@ -15,7 +15,7 @@ const Footer = ()=>{
         <div className="social hidden md:flex fixed top-[50%] z-10">
         <ul className="list-none flex flex-col">
           <motion.li className="mb-5" initial={{ opacity: 0, x: -70 }} animate={{ opacity: 1, x: 0 }} transition={{ duration: 0.4, delay: .75 }} viewport={{ once: true }}>
-            <a href="#" target="_blank" rel="noreferrer"><AiFillGithub size={30} /></a>
+            <a href="https://github.com/" target="_blank" rel="noreferrer"><AiFillGithub size={30} /></a>
           </motion.li>
           <motion.li className="mb-5" initial={{ opacity: 0, x: -70 }} animate={{ opacity: 1, x: 0 }} transition={{ duration: 0.4, delay: 1 }} viewport={{ once: true }}>
             <a href="https://www.linkedin.com/school/irvine-valley-college/" target="_blank" rel="noreferrer"><AiFillLinkedin size={30} /></a>
