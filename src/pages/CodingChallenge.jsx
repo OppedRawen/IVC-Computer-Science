4f431a62bf3111860c35b8c12d2a835f40a2eee2
OppedRawen <yud2372@gmail.com>
2023-09-13 21:53:19 -0700

    more pages

--- src/pages/CodingChallenge.jsx
+++ src/pages/CodingChallenge.jsx
@@ -3,9 +3,9 @@ import React from 'react';
 
 const CodingChallenge = () => {
     return (
-        <div className="container mx-auto px-4 py-12">
-            <h2 className="text-[#54d5bb] font-bold text-3xl mb-6">Coding Challenge Workshops</h2>
-            <p>
+        <div className="container mx-auto px-4 py-12 max-w-screen-lg">
+            <h2 className="flex pt-4 text-[#8892b0] text-2xl sm:text-3xl">Coding Challenge Workshops</h2>
+            <p  className='text-sm sm:text-lg py-5 leading-7 sm:leading-8 text-[#cacaca]'>
                 In each session, we prepare one or two specific programming questions. Contrary to competition-style coding, these workshops are meant to enrich everyone's knowledge and hone their skills in expressing solutions, especially for interview preparations. It's a collaborative environment where everyone actively communicates their solutions and understanding of the challenges presented.
             </p>
         </div>
