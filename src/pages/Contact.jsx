f3b08b61bc5c2e218d6a513eab0f2ac89abd6bdd
wuavonft <kavehm013@gmail.com>
2023-09-19 23:20:39 -0700

    made form fields required and made made the cursor pointer on button hover

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -48,7 +48,7 @@ const Contact = () => {
                         transition={{ duration: 0.5 }}
                     >
                         <label className="block mb-2 text-sm font-medium text-[#8892b0] dark:text-gray-300">Name</label>
-                        <input type="text" name="user_name" id="name" value={name} onChange={(e) => setName(e.target.value)} className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light" />
+                        <input required type="text" name="user_name" id="name" value={name} onChange={(e) => setName(e.target.value)} className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light" />
                     </motion.div>
 
                     <motion.div
@@ -58,7 +58,7 @@ const Contact = () => {
                         transition={{ duration: 0.5, delay: 0.1 }}
                     >
                         <label className="block mb-2 text-sm font-medium text-[#8892b0] dark:text-gray-300">Email</label>
-                        <input type="email" name="user_email" id="email" value={email} onChange={(e) => setEmail(e.target.value)} className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"/>
+                        <input required type="email" name="user_email" id="email" value={email} onChange={(e) => setEmail(e.target.value)} className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"/>
                     </motion.div>
 
                     <motion.div
@@ -68,7 +68,7 @@ const Contact = () => {
                         transition={{ duration: 0.5, delay: 0.2 }}
                     >
                         <label className="block mb-2 text-sm font-medium text-[#8892b0] dark:text-gray-300">Message</label>
-                        <textarea name="message"  id = 'message' onChange={(e) => setMessage(e.target.value)} value ={message} className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"  />
+                        <textarea required name="message"  id = 'message' onChange={(e) => setMessage(e.target.value)} value ={message} className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"  />
                     </motion.div>
 
                     <motion.div
@@ -77,7 +77,7 @@ const Contact = () => {
                         variants={fieldVariants}
                         transition={{ duration: 0.5, delay: 0.3 }}
                     >
-                        <input type="submit" value="Send" className="py-3 px-5 text-sm border border-teal-500 font-medium text-center text-p-light dark:text-p-dark rounded-lg bg-primary-700 sm:w-fit hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800 " />
+                        <input type="submit" value="Send" className="py-3 px-5 text-sm border border-teal-500 font-medium cursor-pointer text-center text-p-light dark:text-p-dark rounded-lg bg-primary-700 sm:w-fit hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800 " />
                     </motion.div>
                 </form>
 
