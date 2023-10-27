09174e9a29de0aae2df46073db4e3a13076d009c
OppedRawen <yud2372@gmail.com>
2023-10-26 23:19:00 -0700

    login ui

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,6 +1,7 @@
 // LoginPage.js
 import React, { useState } from 'react';
 import { auth,signInWithEmailAndPassword } from '../utils/firebaseConfig';
+import { Link } from 'react-router-dom';
 
 function LoginPage() {
     const [email, setEmail] = useState('');
@@ -20,25 +21,60 @@ function LoginPage() {
     };
 
     return (
-        <div>
-            <h2>Login</h2>
-            {error && <p>{error}</p>}
-            <form onSubmit={handleFormSubmit}>
-                <input 
-                    type="email" 
-                    placeholder="Email" 
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)} 
-                />
-                <input 
-                    type="password" 
-                    placeholder="Password"
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)} 
-                />
-                <button type="submit">Login</button>
-            </form>
-        </div>
+        <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
+      <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+        <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Sign in to your account</h2>
+      </div>
+
+      <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm" >
+        <form className="space-y-6" onSubmit={handleFormSubmit}>
+          
+          <div>
+            <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">Email address</label>
+            <div className="mt-2">
+              <input id="email" name="email" type="email" autoComplete="email"  onChange={(e) => setEmail(e.target.value)}  required className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"></input>
+            </div>
+          </div>
+
+          <div>
+            <div className="flex items-center justify-between">
+              <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">Password</label>
+            </div>
+            <div className="mt-2">
+              <input id="password" name="password" type="password" autoComplete="current-password"  onChange={(e) => setPassword(e.target.value)} required className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"></input>
+            </div>
+          </div>
+
+          <div className='flex justify-center'>
+            <button type="submit"  className="flex-col justify-center rounded-md bg-indigo-600 mr-2 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Sign in</button>
+            <Link to='register'>
+            <button type="submit" className="flex-col justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Sign up</button>
+            </Link>
+            
+          </div>
+        </form>
+      </div>
+
+      </div>
+        // <div>
+        //     <h2>Login</h2>
+        //     {error && <p>{error}</p>}
+        //     <form onSubmit={handleFormSubmit}>
+        //         <input 
+        //             type="email" 
+        //             placeholder="Email" 
+        //             value={email}
+        //             onChange={(e) => setEmail(e.target.value)} 
+        //         />
+        //         <input 
+        //             type="password" 
+        //             placeholder="Password"
+        //             value={password}
+        //             onChange={(e) => setPassword(e.target.value)} 
+        //         />
+        //         <button type="submit">Login</button>
+        //     </form>
+        // </div>
     );
 }
 
